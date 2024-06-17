110f34adcf1e2784e75e5b6e504c19c358828147
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-17 23:17:24 +0530

    Update ReceipeCard.jsx

--- src/components/ReceipeCard.jsx
+++ src/components/ReceipeCard.jsx
@@ -1,31 +1,26 @@
 import { Search,Soup,Heart,HeartPulse } from 'lucide-react'
 
-const ReceipeCard = () => {
+const ReceipeCard = ({recipe}) => {
   return (
-    <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden pd-3 relative'>
+    <div className='flex h-30 flex-col rounded-md bg-[#ecf7d4] overflow-hidden pd-3 relative'>
     <a href='#' className='relative  h-22'>
-      <img src="/1.jpg" alt='receipe image'
-      className='rounded-md w-full h-full object-cover cursor-pointer'  />
+      <img src={recipe.image} alt='receipe image'
+      className='rounded-md w-full h-22 object-cover cursor-pointer'  />
       <div className='absolute bottom-2 left-2 bg-white rounded-full pd-1 cursor-pointer flex items-center  gap-1 text-sm'>
-      <Soup size={16} /> 4 servings
+      <Soup size={16} /> {recipe.yield} servings
       </div>
       <div className='absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer'>
         <Heart size={20} className='hover:fill-red-500 hover:text-red-500' />
       </div>
     </a>
     <div className='flex mt-1 '>
-      <p className='font-bold tracking-wide'>Roasted chicken</p>
+      <p className='font-bold tracking-wide'>{recipe.label}</p>
     </div>
-    <p className='my-2'>Turkish kitchen </p>
+    <p className='my-2'>{recipe.cuisineType} </p>
     <div className='flex gap-2 mt-auto'>
-    <div className='flex gap-2 bg-[#d6f497] items-center p-1 rounded-md'>
-    <HeartPulse size={16}/>
-    <span className='text-sm tracking-tighter'> Gluten free</span>
-    </div>
-    <div className='flex gap-2 bg-[#d6f497] items-center p-1 rounded-md'>
-    <HeartPulse size={16}/>
-    <span className='text-sm tracking-tighter'> Keto-diet</span>
-    </div>
+
+
+   
     
     </div>
     </div>
